4b21f51d71c3e422c80a909e60c5d6b59d0a2fe8
Danyal Mohaddes <danmohad@stanford.edu>
2019-07-18 18:05:31 -0700

    Update to fix negative diffusivity issue
    system eigen must be 'n' i.e. the git-shipped version of eigen must be used. This must be used in conjunction with Eigen3.2.9 for CharlesX in order for finite rate chemistry to work. IF you do not do this, the code may compile, may run, and may even give results but there will be GARBAGE in the results due to miscalculation of the diffusive quantities by Cantera.

--- compile_script_nersc_intel.bash
+++ compile_script_nersc_intel.bash
@@ -1,11 +1,11 @@
 #! /bin/bash
 # Need to build SUNDIALS with -fPIC and set(CMAKE_MACOSX_RPATH 1)
 # Need to have configured programming environment to 'intel' (see CharlesX/EnvironmentSetup for NERSC)
+# MUST build Cantera using git-shipped Eigen version, and build CharlesX with a local version 3.2.9
 export CANTERA_DIR=$UTILITIES_ROOT/Cantera/2.4.0
 export SUN_INCLUDE=$UTILITIES_ROOT/Sundials/sundials-2.7.0/include
 export SUN_LIB=$UTILITIES_ROOT/Sundials/sundials-2.7.0/lib
 export BOOST_DIR=$UTILITIES_ROOT/boost/1.55.0
-export EIGEN_INCLUDE=/global/common/cori/software/eigen3/3.3.3/include/eigen3
 
 scons -j20 build prefix=$CANTERA_DIR \
   CXX=CC CC=cc FORTRAN=ftn python_package=none \
@@ -13,10 +13,11 @@ scons -j20 build prefix=$CANTERA_DIR \
   thread_flags='' \
   warning_flags='-Wall' \
   env_vars='all' \
-  sundials_include=$SUN_INCLUDE sundials_libdir=$SUN_LIB extra_inc_dirs=$EIGEN_INCLUDE \
-  boost_inc_dir=$BOOST_DIR/include f90_interface=n system_eigen=y \
+  sundials_include=$SUN_INCLUDE sundials_libdir=$SUN_LIB \
+  boost_inc_dir=$BOOST_DIR/include f90_interface=n system_eigen=n \
   renamed_shared_libraries=n
-# above line may be necessary for -lcantera_shared error in scons test
+# above line necessary for -lcantera_shared error in scons test
+
 if [ -z "SCONS_TEST" ]; then
     scons -j20 test
 fi
